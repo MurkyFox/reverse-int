40a2e35d64b6c9e6875fc3086dd34c0e10019578
MurkyFox <gwy113@gmail.com>
2022-07-19 20:26:48 +0300

    feat: reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+  const nString = n.toString();
+  // console.log(nString);
+  const splitString = nString.split('');
+  // console.log(splitString);
+  const revString = splitString.reverse();
+  // console.log(revString);
+  if (nString[0] != '-') {
+    return revString.join('');
+  } else {
+    const absRevString = revString.pop();
+    return revString.join('');
+  }
 }
+// console.log(module.exports(-251))
